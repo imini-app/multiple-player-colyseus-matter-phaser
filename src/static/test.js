dd6db602a03a7b5a4d3de6798c992914e801d97f
xwillxu <hairblack088@gmail.com>
2022-05-23 14:59:03 -0400

    Create calculations using functions

--- src/static/test.js
+++ src/static/test.js
@@ -1,2 +1,33 @@
-let test = clientsContainer
-console.log(test)
+function calculateAddition(number1, number2) {
+    let answer = number1 + number2
+    return answer
+}
+
+function calculateSubtraction(number1, number2) {
+    let answer = number1 - number2
+    return answer
+}
+
+function calculateMultiplication(number1, number2) {
+    let answer = number1 * number2
+    return answer
+}
+
+function calculateDivision(number1, number2) {
+    let answer = number1 / number2
+    return answer
+}
+
+function calculateAdditionAndMultiplication(number1, number2, number3) {
+    let additionCalculation = calculateAddition(number1, number2)
+    let answer = calculateMultiplication(additionCalculation, number3)
+    return answer
+}
+
+function calculateEquation(baseNumber, numbers, equation) {
+    let currentAnswer = baseNumber
+    for (let number of numbers) {
+        currentAnswer = eval(`${currentAnswer} ${equation} ${number}`)
+    }
+    return currentAnswer
+}
