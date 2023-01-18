18decefb44728b6c6c0cac3bbdbad56ef19eb17b
xwillxu <hairblack088@gmail.com>
2023-01-18 17:50:32 -0500

    Battle.io V1 lay out base code

--- src/rooms/battle-io/GameEngine.ts
+++ src/rooms/battle-io/GameEngine.ts
@@ -3,6 +3,8 @@ export default class GameEngine {
     world = null
     state = null
     engine = null
+    mapWidth = 1920 / 1.32 * 3.6
+    mapHeight = 1920 / 1.32 * 3.6
 
     constructor(roomState) {
         this.engine = Matter.Engine.create()
@@ -15,9 +17,35 @@ export default class GameEngine {
     }
 
     setup() {
-        // TODO: Add logic
+        const boundarys = [
+            // Top wall
+            Matter.Bodies.rectangle(this.mapWidth / 2, 0, this.mapWidth, 50, { isStatic: true }),
+            // Bottom wall
+            Matter.Bodies.rectangle(this.mapWidth / 2, this.mapHeight, this.mapWidth, 50, { isStatic: true, label: 'boundary' }),
+            // Right wall
+            Matter.Bodies.rectangle(this.mapWidth, this.mapHeight / 2, 50, this.mapHeight, { isStatic: true, label: 'boundary' }),
+            // Left wall
+            Matter.Bodies.rectangle(0, this.mapHeight / 2, 50, this.mapHeight, { isStatic: true, label: 'boundary' })
+        ]
+
+        Matter.Composite.add(this.world, boundarys)
     }
-    // TODO: Add other functions
+
+    setupUpdate() {
+        Matter.Events.on(this.engine, "afterUpdate", () => {
+
+        })
+    }
+
+    collision() {
+        Matter.Events.on(this.engine, "collisionStart", (event) => {
+            const bodyPairs = event.pairs;
+            for (const body of bodyPairs) {
+                let bodyA = body.bodyA
+                let bodyB = body.bodyB
+            }
+        })
+    }
 
     processPlayerMovement(sessionId, moveData) {
         // TODO: Add logic
