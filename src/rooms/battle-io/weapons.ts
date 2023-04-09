a2f25c1c21e0bd512baa1708e9e7670c5f2a59dd
xwillxu <hairblack088@gmail.com>
2023-04-09 18:18:19 -0500

    Create test weapon stat

---
+++ src/rooms/battle-io/weapons.ts
@@ -0,0 +1,10 @@
+const weaponStats = {
+    "Double Iron Katana": {
+        ZMasteryUnlock: 1,
+        XMasteryUnlock: 20,
+        damage: 15,
+        reloadSpeed: 0.3,
+        weaponRange: 8,
+        knockback: 10
+    },
+}
